904de96dc89613417b347821eeca2880645bf6bd
nakashimashin <nakashimashin0517@gmail.com>
2024-09-16 23:43:39 +0900

    ailias pathの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Test from '@/components/Test';
 function App() {
 	return (
 		<>
 			<div className="App">
 				<header className="App-header">
+					<Test />
+					<Test />
 					<h1>React TypeScript App</h1>
 					<div>aa</div>
 				</header>
