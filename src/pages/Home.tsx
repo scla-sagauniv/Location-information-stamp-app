3661d8647fca944795abe73c41fc26319d95184f
nakashimashin <nakashimashin0517@gmail.com>
2024-09-21 23:10:31 +0900

    使用しているブラウザがweb bluetooth apiが使用可能かを判別

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+
 function Home() {
+	useEffect(() => {
+		navigator.bluetooth.getAvailability().then((available) => {
+			if (available) {
+				console.log('Web Bluetooth is available');
+			} else {
+				console.log('Web Bluetooth is not available');
+			}
+		});
+	}, []);
+
 	return (
 		<>
 			<div>Home</div>
