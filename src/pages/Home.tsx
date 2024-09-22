c23193d2765db59fa050373c511ffc27f994c088
nakashimashin <nakashimashin0517@gmail.com>
2024-09-22 18:35:13 +0900

    navigator.bluetoothがundefinedの場合の対応

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,22 @@ import { useEffect } from 'react';
 
 function Home() {
 	useEffect(() => {
-		navigator.bluetooth.getAvailability().then((available) => {
-			if (available) {
-				console.log('Web Bluetooth is available');
-			} else {
-				console.log('Web Bluetooth is not available');
-			}
-		});
+		if (navigator.bluetooth) {
+			navigator.bluetooth
+				.getAvailability()
+				.then((available) => {
+					if (available) {
+						console.log('Web Bluetooth is available');
+					} else {
+						console.log('Web Bluetooth is not available');
+					}
+				})
+				.catch((error) => {
+					console.error('Error accessing Bluetooth API:', error);
+				});
+		} else {
+			console.log('Web Bluetooth API is not available in this browser.');
+		}
 	}, []);
 
 	return (
