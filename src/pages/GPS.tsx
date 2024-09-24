8c9562c0ffb26859dbdd17b952cbb604113f6525
nakashimashin <nakashimashin0517@gmail.com>
2024-09-25 00:12:31 +0900

    円形のUI

--- src/pages/GPS.tsx
+++ src/pages/GPS.tsx
@@ -1,25 +1,12 @@
-import { useGeolocation } from '../hooks/useGeolocation';
-import { Button } from '@/components/ui/button';
-
 function GPS() {
-	const targetLocation = {
-		lat: 33.2382305,
-		lon: 130.2962176,
-	};
-	const radius = 30;
-
-	const isWithinRadius = useGeolocation(targetLocation, radius);
-
 	return (
-		<>
-			<div className="text-blue-600">
-				{isWithinRadius
-					? 'スタンプ獲得!!'
-					: 'スタンプ獲得まで30m圏内に入ってください'}
+		<div className="h-screen w-screen flex items-center justify-center">
+			<div className="radar-container">
+				<div className="radar-bg">
+					<div className="radar-scan-line"></div>
+				</div>
 			</div>
-			<div className="text-blue-700 text-xl">aaaa</div>
-			<Button>スタンプを獲得する</Button>
-		</>
+		</div>
 	);
 }
 
