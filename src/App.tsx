18861b59a06648a117b205de43b047704f7b91a7
nakashimashin <nakashimashin0517@gmail.com>
2024-10-08 23:44:22 +0900

    余計なページを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '@/pages/Home';
 import GPS from '@/pages/GPS';
 import Page404 from '@/pages/Page404';
 
@@ -10,7 +9,6 @@ function App() {
 			<div className="App">
 				<BrowserRouter>
 					<Routes>
-						<Route path="/gps" element={<Home />} />
 						<Route path="/" element={<GPS />} />
 						<Route path="*" element={<Page404 />} />
 					</Routes>
