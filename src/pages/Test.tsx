a49f4398df1aa1f7ae7dd08f86325d1a2349db10
dorapon48 <dorapon48@icloud.com>
2024-10-14 18:14:17 +0900

    update: マップの位置を調整

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -17,65 +17,43 @@ const Block: React.FC<BlockProps> = ({ className, label }) => {
 
 function Test() {
 	return (
-		<div className="flex justify-center items-center h-screen bg-gray-100">
-			<div className="relative w-[600px] h-[844px] border-2 border-black">
-				{/* 図書館 */}
-				<Block className="absolute left-0 bottom-0 w-32 h-32" label="図書館" />
-
-				{/* ステージ */}
-				<Block
-					className="absolute left-0 bottom-32 w-32 h-48"
-					label="ステージ"
-				/>
-
-				{/* 通路 */}
-				<Block
-					className="absolute left-32 bottom-32 w-[300px] h-16"
-					label="通路"
-				/>
-
-				{/* 古着屋 */}
-				<Block
-					className="absolute left-32 bottom-48 w-32 h-16"
-					label="古着屋"
-				/>
-
-				{/* バザー 1 */}
-				<Block
-					className="absolute left-64 bottom-32 w-32 h-16"
-					label="バザー"
-				/>
-
-				{/* キッチンカーなど */}
-				<Block
-					className="absolute right-0 bottom-0 w-32 h-32"
-					label="キッチンカーなど"
-				/>
-
-				{/* バザー 2 */}
-				<Block
-					className="absolute right-0 bottom-32 w-32 h-64"
-					label="バザー"
-				/>
-
-				{/* 銅像 */}
-				<Block className="absolute right-0 bottom-96 w-32 h-16" label="銅像" />
-
-				{/* 教養1号館 */}
-				<Block
-					className="absolute right-0 top-96 w-32 h-32"
-					label="教養1号館"
-				/>
-
-				{/* 教養2号館 */}
-				<Block
-					className="absolute right-0 top-64 w-32 h-32"
-					label="教養2号館"
-				/>
-
-				{/* 美術館 */}
-				<Block className="absolute right-40 top-64 w-32 h-32" label="美術館" />
-			</div>
+		<div className="flex overflow-x-auto bg-gray-100 h-screen w-[884px]">
+			<Block
+				className="absolute left-[64px] bottom-[64px] w-[128px] h-[128px]"
+				label="図書館"
+			/>
+			<Block
+				className="absolute left-[64px] bottom-[256px] w-[128px] h-[256px]"
+				label="ステージ"
+			/>
+			<Block
+				className="absolute left-[64px] bottom-[192px] w-[300px] h-[64px]"
+				label="通路"
+			/>
+			<Block
+				className="absolute left-[364px] bottom-[192px] w-[200px] h-[64px]"
+				label="バザー"
+			/>
+			<Block
+				className="absolute left-[564px] bottom-[192px] w-[64px] h-[600px]"
+				label="バザー"
+			/>
+			<Block
+				className="absolute left-[436px] bottom-[64px] w-[192px] h-[128px]"
+				label="キッチンカーなど"
+			/>
+			<Block
+				className="absolute left-[628px] bottom-[664px] w-[192px] h-[128px]"
+				label="教養2号館"
+			/>
+			<Block
+				className="absolute left-[628px] bottom-[536px] w-[192px] h-[128px]"
+				label="教養1号館"
+			/>
+			<Block
+				className="absolute left-[436px] bottom-[600px] w-[128px] h-[192px]"
+				label="美術館"
+			/>
 		</div>
 	);
 }
