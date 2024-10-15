21a0384e4481274fc634813845254e75fca2e0b6
dorapon48 <dorapon48@icloud.com>
2024-10-15 18:20:31 +0900

    update: 概形を整えた

--- src/api/useIsUserInArea.ts
+++ src/api/useIsUserInArea.ts
@@ -1,1 +1,16 @@
 // 指定したエリアに指定した座標が存在するか判定する
+
+type Props = {
+	area: object;
+	position: object;
+};
+
+// 地図のブロック
+const useIsUserInArea = ({ area, position }: Props) => {
+	if (area && position) {
+		return false;
+	}
+	return true;
+};
+
+export default useIsUserInArea;
