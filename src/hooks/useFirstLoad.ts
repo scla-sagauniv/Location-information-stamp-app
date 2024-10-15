1c7353845a4e3214f42471e788b2bca12dcd12da
nakashimashin <nakashimashin0517@gmail.com>
2024-10-15 18:50:59 +0900

    fix: useEffectを一つにまとめる

--- src/hooks/useFirstLoad.ts
+++ src/hooks/useFirstLoad.ts
@@ -1,25 +1,32 @@
 import { useState, useEffect } from 'react';
 import { Location } from '@/types';
+import { watchGeolocation } from '@/utils/watchGeolocation';
 
 export const useFirstLoad = (): [
-	boolean,
-	(open: boolean) => void,
-	Location[],
+	isExplanationOpen: boolean,
+	setIsExplanationOpen: (open: boolean) => void,
+	selectedLocations: Location[],
+	isWithinRadius: boolean[],
+	altitude: number | null,
 ] => {
+	const radius = 30;
 	const [isExplanationOpen, setIsExplanationOpen] = useState<boolean>(false);
 	const [selectedLocations, setSelectedLocations] = useState<Location[]>([]);
+	const [isWithinRadius, setIsWithinRadius] = useState<boolean[]>([]);
+	const [altitude, setAltitude] = useState<number | null>(null);
 
 	useEffect(() => {
+		// モーダル表示チェック
 		const hasSeenExplanation = localStorage.getItem('isModalShown');
-
 		if (!hasSeenExplanation) {
 			setIsExplanationOpen(true);
 			localStorage.setItem('isModalShown', 'true');
 		}
 
 		const savedLocations = localStorage.getItem('selectedLocations');
+		let locations: Location[];
 		if (savedLocations) {
-			setSelectedLocations(JSON.parse(savedLocations));
+			locations = JSON.parse(savedLocations);
 		} else {
 			const targetLocations: Location[] = [
 				{ lat: 33.2382305, lon: 130.2962176, name: '佐賀大学' },
@@ -29,12 +36,32 @@ export const useFirstLoad = (): [
 				{ lat: 33.24, lon: 130.297, name: '大分大学' },
 				{ lat: 33.2382305, lon: 130.2962176, name: '九州大学' },
 			];
-			const shuffledLocations = targetLocations.sort(() => 0.5 - Math.random());
-			const selected = shuffledLocations.slice(0, 3);
-			setSelectedLocations(selected);
-			localStorage.setItem('selectedLocations', JSON.stringify(selected));
+			const randomLocations = targetLocations.sort(() => 0.5 - Math.random());
+			locations = randomLocations.slice(0, 3);
+			localStorage.setItem('selectedLocations', JSON.stringify(locations));
+		}
+		setSelectedLocations(locations);
+
+		// selectedLocations がセットされてから watchGeolocation を呼び出す
+		if (locations.length > 0) {
+			const watchIds = watchGeolocation(
+				locations,
+				radius,
+				setIsWithinRadius,
+				setAltitude,
+			);
+
+			return () => {
+				watchIds.forEach((id) => navigator.geolocation.clearWatch(id));
+			};
 		}
 	}, []);
 
-	return [isExplanationOpen, setIsExplanationOpen, selectedLocations];
+	return [
+		isExplanationOpen,
+		setIsExplanationOpen,
+		selectedLocations,
+		isWithinRadius,
+		altitude,
+	];
 };
