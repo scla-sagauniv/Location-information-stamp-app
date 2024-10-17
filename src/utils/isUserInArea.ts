58f0f2a9824005e30f784f44f754581a3c2005ed
dorapon48 <dorapon48@icloud.com>
2024-10-17 22:54:43 +0900

    update: useStateを削除し再構成

--- src/utils/isUserInArea.ts
+++ src/utils/isUserInArea.ts
@@ -1,43 +1,24 @@
 import * as turf from '@turf/turf';
 import { Location } from '@/types';
-import { useState } from 'react';
+import { Position } from 'geojson';
 // 指定したエリアに指定した座標が存在するか判定する
 
 const isUserInArea = (area: Location[], location: Location): boolean => {
 	// 多角形を定義する4点の緯度経度
-	// 図書館
-	// (33.2418049, 130.2906834)
-	// (33.2412766, 130.2908134)
-	// (33.2413686, 130.2914200)
-	// (33.2418741, 130.2913231)
-	//
-	// 通路 33.2419183, 130.2911011
-	// 図書館 33.2415736, 130.2911185
-	// const polygonPoints = [
-	// 	[130.2906834, 33.2418049], // 点1 (東京)
-	// 	[130.2908134, 33.2412766], // 点2
-	// 	[130.29142, 33.2413686], // 点3
-	// 	[130.2913231, 33.2418741], // 点4
-	// ];
-	const [polygonPoints] = useState<number[][]>([]);
+	const polygonPoints: Position[] = [];
 	for (let i = 0; i < area.length; i++) {
-		const t = [area[i].lon, area[i].lat];
-		polygonPoints.push(t);
+		const tp: Position = [area[i].lon, area[i].lat];
+		polygonPoints.push(tp);
 	}
 	polygonPoints.push(polygonPoints[0]);
 	console.log(polygonPoints);
-	//console.log(area);
-	// 判定する点
-	// const lat = 33.2419183; // 初期値は東京の緯度
-	// const lon = 130.2911011; // 初期値は東京の経度
-	let isInside = null;
 
 	const polygon = turf.polygon([polygonPoints]); // 多角形を定義
 	const point = turf.point([location.lon, location.lat]); // 判定する点を定義
+	console.log(point);
 
 	// 範囲内判定
-	const inside = turf.booleanPointInPolygon(point, polygon);
-	isInside = inside;
+	const isInside = turf.booleanPointInPolygon(point, polygon);
 
 	return isInside;
 };
